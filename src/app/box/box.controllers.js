a9fe80c2ebbb1f672d10e28e2c7cd12e4ad87f9f
sOm2y <yyin888@aucklanduni.ac.nz>
2016-10-29 19:58:18 +1300

    box init

---
+++ src/app/box/box.controllers.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fruitworldCustomerWebsite')
+    .controller('BoxController', BoxController);
+
+  /** @ngInject */
+  function BoxController() {
+    var vm = this;
+
+  }
+})();
