b3b7f8e8c3fe5f057478bfcbe1464e07216eba3b
sOm2y <yyin888@aucklanduni.ac.nz>
2016-10-29 19:57:20 +1300

    init shoppingcart

---
+++ src/app/shoppingCart/shoppingCart.controllers.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fruitworldCustomerWebsite')
+    .controller('ShoppingCartController', ShoppingCartController);
+
+  /** @ngInject */
+  function ShoppingCartController() {
+    var vm = this;
+
+  }
+})();
