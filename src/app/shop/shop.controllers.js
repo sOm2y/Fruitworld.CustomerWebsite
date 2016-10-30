9126d257ccafcba48cd98567c57f32ed0dbc8f91
sOm2y <yyin888@aucklanduni.ac.nz>
2016-10-30 20:21:19 +1300

    shop/shop details changes

--- src/app/shop/shop.controllers.js
+++ src/app/shop/shop.controllers.js
@@ -8,29 +8,6 @@
   /** @ngInject */
   function ShopController(fruitWorldAPIService,$state) {
     var vm = this;
-
-    fruitWorldAPIService.query({
-        section: 'products/read/'
-      })
-      .$promise.then(function(res) {
-        console.log(res);
-        vm.products = res;
-      }, function(err) {
-        console.log(err);
-      });
-    fruitWorldAPIService.query({
-        section: 'products/category/'
-      })
-      .$promise.then(function(res) {
-        console.log(res);
-        vm.categories = res;
-      }, function(err) {
-        console.log(err);
-      });
-      vm.selectProduct = function(product){
-        vm.selectedProduct = product;
-        console.log(vm.selectedProduct);
-        $state.go('shop.details');
-      };
+      
   }
 })();
--- src/app/shopDetails/shopDetails.controllers.js
+++ src/app/shopDetails/shopDetails.controllers.js
@@ -8,23 +8,6 @@
   /** @ngInject */
   function ShopDetailsController(fruitWorldAPIService) {
     var vm = this;
-    fruitWorldAPIService.query({
-        section: 'products/read/'
-      })
-      .$promise.then(function(res) {
-        console.log(res);
-        vm.products = res;
-      }, function(err) {
-        console.log(err);
-      });
-      fruitWorldAPIService.query({
-          section: 'products/category/'
-        })
-        .$promise.then(function(res) {
-          console.log(res);
-          vm.categories = res;
-        }, function(err) {
-          console.log(err);
-        });
+    vm.selectedProduct = JSON.parse(localStorage.getItem('selectedProduct'));
   }
 })();
