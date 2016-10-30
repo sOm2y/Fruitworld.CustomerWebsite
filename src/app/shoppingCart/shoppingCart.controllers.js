b5f341103fa54bd9fc6d4c1847c329346eb107d8
sOm2y <yyin888@aucklanduni.ac.nz>
2016-10-30 20:21:03 +1300

    shopping cart implementation

--- src/app/shoppingCart/shoppingCart.controllers.js
+++ src/app/shoppingCart/shoppingCart.controllers.js
@@ -6,8 +6,25 @@
     .controller('ShoppingCartController', ShoppingCartController);
 
   /** @ngInject */
-  function ShoppingCartController() {
+  function ShoppingCartController(shoppingCartService) {
     var vm = this;
-
+    vm.countedShoppingCart = shoppingCartService.getShoppingCart();
+    vm.hasShoppingCartChanged = false;
+    vm.countedShoppingCart = shoppingCartService.getShoppingCart();
+    vm.addToBox = function(products) {
+      //TODO
+    };
+    vm.updateProductCount = function(index, updatedProduct) {
+      vm.countedShoppingCart[index].count = updatedProduct.count;
+      shoppingCartService.getTotalPrice(vm.countedShoppingCart);
+      vm.hasShoppingCartChanged = true;
+    };
+    vm.updateShoppingCart = function() {
+      localStorage.setItem('countedShoppingCart', JSON.stringify(vm.countedShoppingCart));
+    };
+    vm.deleteCartProduct = function(deleteProduct) {
+      vm.countedShoppingCart = shoppingCartService.removeProduct(deleteProduct, vm.countedShoppingCart);
+      shoppingCartService.getTotalPrice(vm.countedShoppingCart);
+    };
   }
 })();
