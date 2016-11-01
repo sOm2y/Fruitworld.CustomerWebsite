607cd10ea4132e462cfcf4031d524641b116c7f9
sOm2y <yyin888@aucklanduni.ac.nz>
2016-11-01 20:22:31 +1300

    init payment

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -17,20 +17,51 @@
       .state('home.shop', {
         url: 'shop',
 
-            templateUrl: 'app/shop/shop.html',
-            controller: 'ShopController',
-            controllerAs: 'shop'
+        templateUrl: 'app/shop/shop.html',
+        controller: 'ShopController',
+        controllerAs: 'shop'
 
 
       })
       .state('home.account', {
         url: 'account',
 
-            templateUrl: 'app/account/account.html',
-            controller: 'AccountController',
-            controllerAs: 'account'
+        templateUrl: 'app/account/account.html',
+        controller: 'AccountController',
+        controllerAs: 'account'
 
 
+      })
+      .state('home.payment', {
+        url: 'payment',
+        templateUrl: 'app/payment/payment.html',
+        controller: 'PaymentController',
+        controllerAs: 'payment',
+        deepStateRedirect: {
+          default: {
+            state: 'home.payment.address'
+          }
+        },
+      })
+      .state('home.payment.address', {
+        url: '/address',
+        templateUrl: 'app/payment/payment-address.html'
+      })
+      .state('home.payment.billing', {
+        url: '/billing',
+        templateUrl: 'app/payment/payment-billing.html'
+      })
+      .state('home.payment.shipping', {
+        url: '/shipping',
+        templateUrl: 'app/payment/payment-shipping.html'
+      })
+      .state('home.payment.method', {
+        url: '/method',
+        templateUrl: 'app/payment/payment-method.html'
+      })
+      .state('home.payment.order', {
+        url: '/order',
+        templateUrl: 'app/payment/payment-order.html'
       })
       .state('home.shop.details', {
         url: '/details',
@@ -43,12 +74,12 @@
         }
       })
 
-      .state('home.box', {
+    .state('home.box', {
         url: 'box',
 
-            templateUrl: 'app/box/box.html',
-            controller: 'BoxController',
-            controllerAs: 'box'
+        templateUrl: 'app/box/box.html',
+        controller: 'BoxController',
+        controllerAs: 'box'
 
 
       })
@@ -59,15 +90,15 @@
             templateUrl: 'app/box/boxDetails.html',
             controller: 'BoxController',
             controllerAs: 'boxDetails'
-            }
           }
+        }
 
       }).state('home.contact', {
         url: 'contact',
 
-            templateUrl: 'app/contact/contact.html',
-            controller: 'ContactController',
-            controllerAs: 'contact'
+        templateUrl: 'app/contact/contact.html',
+        controller: 'ContactController',
+        controllerAs: 'contact'
 
 
 
@@ -75,9 +106,9 @@
       .state('home.shoppingCart', {
         url: 'shopping-cart',
 
-            templateUrl: 'app/shoppingCart/shoppingCart.html',
-            controller: 'ShoppingCartController',
-            controllerAs: 'shoppingCart'
+        templateUrl: 'app/shoppingCart/shoppingCart.html',
+        controller: 'ShoppingCartController',
+        controllerAs: 'shoppingCart'
 
 
 
---
+++ src/app/payment/payment.controllers.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fruitworldCustomerWebsite')
+    .controller('PaymentController', PaymentController);
+
+  /** @ngInject */
+  function PaymentController() {
+    var vm = this;
+  }
+})();
