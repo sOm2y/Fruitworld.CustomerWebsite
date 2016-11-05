68e51eafd053a3a1dc053dfd3060271943488fe6
sOm2y <yyin888@aucklanduni.ac.nz>
2016-11-05 19:46:31 +1300

    braintree payment setup

--- src/app/payment/payment.controllers.js
+++ src/app/payment/payment.controllers.js
@@ -6,7 +6,51 @@
     .controller('PaymentController', PaymentController);
 
   /** @ngInject */
-  function PaymentController() {
+  function PaymentController($http) {
     var vm = this;
+    $http.get('http://f2appspaymentdev.azurewebsites.net/api/TransactionClients/token').then(function(res) {
+        console.log(res);
+				braintree.setup(res.data, "dropin", {
+					container: "payment-form",
+					paymentMethodNonceReceived: function (event, nonce) {
+
+						vm.message = 'Processing your payment...';
+						vm.showDropinContainer = false;
+
+						$http({
+							method: 'POST',
+							url: 'http://f2appspaymentdev.azurewebsites.net/api/TransactionClients/create',
+							data: {
+								amount: vm.amount,
+								payment_method_nonce: nonce
+							}
+						}).success(function (data) {
+							vm.transation = data;
+							console.log(data.success);
+
+							if (data.success) {
+								vm.message = 'Payment authorized, thanks.';
+								vm.showDropinContainer = false;
+								vm.isError = false;
+								vm.isPaid = true;
+
+							} else {
+								// implement your solution to handle payment failures
+								vm.message = 'Payment failed: ' + data.message + ' Please refresh the page and try again.';
+								vm.isError = true;
+							}
+
+						}).error(function (error) {
+							vm.message = 'Error: cannot connect to server. Please make sure your server is running.';
+							vm.showDropinContainer = false;
+							vm.isError = true;
+						});
+
+					}
+				});
+			});
+
+
+
   }
 })();
