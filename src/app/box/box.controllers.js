9c208eeaa3e8f2260e739904841ab3e392f4a1c0
sOm2y <yyin888@aucklanduni.ac.nz>
2016-11-08 20:27:14 +1300

    add pagaination and loop box out

--- src/app/box/box.controllers.js
+++ src/app/box/box.controllers.js
@@ -6,8 +6,36 @@
     .controller('BoxController', BoxController);
 
   /** @ngInject */
-  function BoxController() {
+  function BoxController(fruitWorldAPIService, $scope) {
     var vm = this;
+    fruitWorldAPIService.query({
+        section: 'box/read/'
+      })
+      .$promise.then(function(res) {
+        console.log(res);
+        vm.boxes = res,
+        vm.filteredBoxes = [],
+        vm.currentPage = 1,
+        vm.numPerPage = 12,
+        vm.maxSize = 5;
 
+
+        vm.pageChanged = function(){
+          var begin = ((vm.currentPage - 1) * vm.numPerPage),
+            end = begin + vm.numPerPage;
+
+          vm.filteredBoxes = vm.boxes.slice(begin, end);
+        };
+        vm.pageChanged();
+
+        vm.selectProduct = function(box) {
+          vm.selectedBox = box;
+          localStorage.setItem('selectedBox', JSON.stringify(vm.selectedBox));
+          console.log(vm.selectedBox);
+          $state.go('home.box.details');
+        };
+      }, function(err) {
+        console.log(err);
+      });
   }
 })();
