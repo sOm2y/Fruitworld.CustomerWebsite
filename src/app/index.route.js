b2072b241630af50cef30f0a21b37ad45ffe8694
sOm2y <yyin888@aucklanduni.ac.nz>
2016-11-12 23:25:05 +1300

    add pages requirelogin

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -12,56 +12,72 @@
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
-        controllerAs: 'main'
+        controllerAs: 'main',
+        data: {
+            requireLogin: false
+        }
       })
       .state('home.shop', {
         url: 'shop',
-
         templateUrl: 'app/shop/shop.html',
         controller: 'ShopController',
-        controllerAs: 'shop'
-
-
+        controllerAs: 'shop',
+        data: {
+            requireLogin: false
+        }
       })
       .state('home.account', {
         url: 'account',
-
         templateUrl: 'app/account/account.html',
         controller: 'AccountController',
-        controllerAs: 'account'
-
-
+        controllerAs: 'account',
+        data: {
+            requireLogin: false
+        }
       })
       .state('home.payment', {
         url: 'payment',
         templateUrl: 'app/payment/payment.html',
         controller: 'PaymentController',
         controllerAs: 'payment',
-        deepStateRedirect: {
-          default: {
-            state: 'home.payment.address'
-          }
-        },
+        data: {
+            requireLogin: true
+        }
       })
       .state('home.payment.address', {
         url: '/address',
-        templateUrl: 'app/payment/payment-address.html'
+        templateUrl: 'app/payment/payment-address.html',
+        data: {
+            requireLogin: true
+        }
       })
       .state('home.payment.billing', {
         url: '/billing',
-        templateUrl: 'app/payment/payment-billing.html'
+        templateUrl: 'app/payment/payment-billing.html',
+        data: {
+            requireLogin: true
+        }
       })
       .state('home.payment.shipping', {
         url: '/shipping',
-        templateUrl: 'app/payment/payment-shipping.html'
+        templateUrl: 'app/payment/payment-shipping.html',
+        data: {
+            requireLogin: true
+        }
       })
       .state('home.payment.method', {
         url: '/method',
-        templateUrl: 'app/payment/payment-method.html'
+        templateUrl: 'app/payment/payment-method.html',
+        data: {
+            requireLogin: true
+        }
       })
       .state('home.payment.order', {
         url: '/order',
-        templateUrl: 'app/payment/payment-order.html'
+        templateUrl: 'app/payment/payment-order.html',
+        data: {
+            requireLogin: true
+        }
       })
       .state('home.shop.details', {
         url: '/details',
@@ -71,17 +87,20 @@
             controller: 'ShopDetailsController',
             controllerAs: 'shopDetails'
           }
+        },
+        data: {
+            requireLogin: false
         }
       })
 
     .state('home.box', {
         url: 'box',
-
         templateUrl: 'app/box/box.html',
         controller: 'BoxController',
-        controllerAs: 'box'
-
-
+        controllerAs: 'box',
+        data: {
+            requireLogin: false
+        }
       })
       .state('home.box.details', {
         url: '/details',
@@ -91,27 +110,27 @@
             controller: 'BoxController',
             controllerAs: 'boxDetails'
           }
+        },
+        data: {
+            requireLogin: false
         }
-
       }).state('home.contact', {
         url: 'contact',
-
         templateUrl: 'app/contact/contact.html',
         controller: 'ContactController',
-        controllerAs: 'contact'
-
-
-
+        controllerAs: 'contact',
+        data: {
+            requireLogin: false
+        }
       })
       .state('home.shoppingCart', {
         url: 'shopping-cart',
-
         templateUrl: 'app/shoppingCart/shoppingCart.html',
         controller: 'ShoppingCartController',
-        controllerAs: 'shoppingCart'
-
-
-
+        controllerAs: 'shoppingCart',
+        data: {
+            requireLogin: false
+        }
       });
 
     $urlRouterProvider.otherwise('/');
