8f9564a471b0d36db89b4cc5ff3e3b5de016541c
sOm2y <yyin888@aucklanduni.ac.nz>
2016-11-15 03:27:17 +0000

    add auckland centerl google map coordianate

--- src/app/contact/contact.controllers.js
+++ src/app/contact/contact.controllers.js
@@ -10,9 +10,13 @@
     var vm = this;
      $rootScope.isLoading = false;
      var locations = [
-    ['Location 1 Name', 'New York, NY', 'Location 1 URL'],
-    ['Location 2 Name', 'Newark, NJ', 'Location 2 URL'],
-    ['Location 3 Name', 'Philadelphia, PA', 'Location 3 URL']
+    ['Fruit World Glen Innes', '185 Apirana Avenue, Glen Innes,Auckland Central', ''],
+    ['Fruit World Greenlane', '1 Marewa Road, Greenlane, Auckland Central', ''],
+    ['Fruit World Mt Albert', '955 New North Road, Mt Albert, Auckland Central', ''],
+    ['Fruit World Mt Roskill', '30A Frost Road, Mt Roskill, Auckland Central', ''],
+    ['Fruit World Mt Wellington', 'Mt Wellington Shopping Centre, Penrose', ''],
+    ['Fruit World Ponsonby', 'Unit 4, 280 Richmond Road, Ponsonby, Auckland Central', ''],
+    ['Supa Royal oak', '100 pah road, royal oak, Auckland Central', '']
 ];
 
 var geocoder;
@@ -22,7 +26,7 @@ var bounds = new google.maps.LatLngBounds();
 function initialize() {
     map = new google.maps.Map(
     document.getElementById("googleMap"), {
-        center: new google.maps.LatLng(37.4419, -122.1419),
+        center: new google.maps.LatLng(-36.862897, 174.758704),
         zoom: 13,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
@@ -67,7 +71,7 @@ function geocodeAddress(locations, i) {
 function infoWindow(marker, map, title, address, url) {
     google.maps.event.addListener(marker, 'click', function () {
         var html = "<div><h3>" + title + "</h3><p>" + address + "<br></div><a href='" + url + "'>View location</a></p></div>";
-        iw = new google.maps.InfoWindow({
+        var iw = new google.maps.InfoWindow({
             content: html,
             maxWidth: 350
         });
