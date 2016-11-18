b3ddf00ea9faa18a49a497074bf37716bf98f0af
sOm2y <yyin888@aucklanduni.ac.nz>
2016-11-18 02:49:06 +0000

    implementing sendgrid email service

--- src/app/index.services.js
+++ src/app/index.services.js
@@ -203,6 +203,27 @@
           });
         }
         
-    }]);
+    }])
+    .factory('sendgridEmailService', ['$http',
+      function($http) {
+          return {
+              send: function(api_user, api_key, to, toname, subject, text, from) {
+                  var method = 'GET';
+                  var url = "https://api.sendgrid.com/api/mail.send.json?";
+                  $http({
+                      method: method,
+                      url: url + "api_user=" + api_user +
+                          "&api_key=" + api_key +
+                          "&to=" + to +
+                          "&subject=" + subject +
+                          "&text=" + text +
+                          "&from=" + from
+                  }).
+                  success(function(data, status) {}).
+                  error(function(data, status) {});
+              }
+          };
+      }
+  ]);
 
 })();
